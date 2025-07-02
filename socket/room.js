2a5df941d3d8c535ffa88b141e32ff72ddf84e89
sai-d-kumar-balu <balusaikumar1233@gmail.com>
2025-07-02 21:02:48 +0530

    changed socket room code

--- socket/room.js
+++ socket/room.js
@@ -1,35 +1,41 @@
-const { Server } = require("socket.io");
+const { Server } = require('socket.io');
 
 module.exports = (server) => {
     const io = new Server(server, {
         cors: {
-            origin: "*",
+            origin: '*',
         },
     });
 
-    io.on("connection", (socket) => {
-        console.log("User connected:", socket.id);
+    io.on('connection', (socket) => {
+        console.log('User connected:', socket.id);
 
-        socket.on("join-room", (roomId) => {
-            socket.join(roomId);
-            socket.to(roomId).emit("user-joined", socket.id);
-            console.log(`${socket.id} joined room ${roomId}`);
+        // When someone initiates a call
+        socket.on('initiateCall', ({ userId, signalData, myId }) => {
+            console.log(`Call initiated from ${myId} to ${userId}`);
+            io.to(userId).emit('incomingCall', {
+                from: myId,
+                signalData,
+            });
         });
 
-        socket.on("offer", ({ roomId, offer }) => {
-            socket.to(roomId).emit("offer", { offer, from: socket.id });
+        // When receiver answers the call
+        socket.on('answerCall', ({ signal, to }) => {
+            console.log(`${socket.id} answered call from ${to}`);
+            io.to(to).emit('callAccepted', signal);
         });
 
-        socket.on("answer", ({ roomId, answer }) => {
-            socket.to(roomId).emit("answer", { answer, from: socket.id });
+        // End call
+        socket.on('endCall', ({ to }) => {
+            console.log(`${socket.id} ended call with ${to}`);
+            io.to(to).emit('callEnded');
         });
 
-        socket.on("ice-candidate", ({ roomId, candidate }) => {
-            socket.to(roomId).emit("ice-candidate", { candidate, from: socket.id });
-        });
+        // Optional: emit your ID to clients (useful if frontend doesn’t have it yet)
+        socket.emit('yourID', socket.id);
 
-        socket.on("disconnect", () => {
-            console.log("User disconnected:", socket.id);
+        socket.on('disconnect', () => {
+            console.log('User disconnected:', socket.id);
         });
     });
 };
